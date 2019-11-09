569bc6dd46f4dea8f13a0ffe883a52bf3f620a53
vgabor96 <loahclan96@gmail.com>
2019-11-09 17:43:10 +0100

    SportsBettingService interface created

--- src/com/sportsbetting/App.java
+++ src/com/sportsbetting/App.java
@@ -1,6 +1,6 @@
 package com.sportsbetting;
 
-public class Main {
+public class App {
 
     public static void main(String[] args) {
 	// write your code here
---
+++ src/com/sportsbetting/SportsBettingService.java
@@ -0,0 +1,4 @@
+package com.sportsbetting;
+
+public interface SportsBettingService {
+}
