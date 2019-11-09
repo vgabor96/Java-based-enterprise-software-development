c2f21a53d9c4db86d70875488aed09f321cfdb04
vgabor96 <loahclan96@gmail.com>
2019-11-09 17:34:28 +0100

    Player class Added

---
+++ src/com/sportsbetting/domain/Player.java
@@ -0,0 +1,12 @@
+package com.sportsbetting.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Player {
+    String name;
+    Integer accountNumber;
+    BigDecimal balance;
+    LocalDate birth;
+    Currency currency;
+}
