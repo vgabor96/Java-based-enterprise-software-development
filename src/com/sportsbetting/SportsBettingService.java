ac46a22ddab03de4fda3992a37b482042d4a2442
vgabor96 <loahclan96@gmail.com>
2019-11-09 17:46:19 +0100

    SportsBettingService interface filled

--- src/com/sportsbetting/SportsBettingService.java
+++ src/com/sportsbetting/SportsBettingService.java
@@ -1,4 +1,17 @@
 package com.sportsbetting;
 
+import com.sportsbetting.domain.Player;
+import com.sportsbetting.domain.SportEvent;
+import com.sportsbetting.domain.Wager;
+
+import java.util.List;
+
 public interface SportsBettingService {
+    void savePlayer(Player player);
+    Player findPlayer();
+    List<SportEvent> findAllSportEvents();
+    void saveWager(Wager wager);
+    List<Wager>findAllWagers();
+    void CalculateResults();
+
 }
