47c076d6a9cc9ad97082e5ff2f243acadf6a991f
vgabor96 <loahclan96@gmail.com>
2019-11-09 17:33:27 +0100

    Wager class Added

---
+++ src/com/sportsbetting/domain/Wager.java
@@ -0,0 +1,12 @@
+package com.sportsbetting.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Wager {
+    BigDecimal amount;
+    LocalDateTime timestampCreated;
+    boolean processed;
+    boolean win;
+    OutcomeOdd odd;
+}
