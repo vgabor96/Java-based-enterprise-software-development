552c7252463b99067bdf729bf0c30a24aa9665e1
vgabor96 <loahclan96@gmail.com>
2019-11-09 17:26:29 +0100

    SportEvent Bet class Added

---
+++ src/com/sportsbetting/domain/SportEvent.java
@@ -0,0 +1,11 @@
+package com.sportsbetting.domain;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class SportEvent {
+    String title;
+    LocalDateTime startDate;
+    LocalDateTime endDate;
+    List<Bet> bets;
+}
