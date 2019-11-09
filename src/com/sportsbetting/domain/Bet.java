00907d2477707c044da2ac781aee7ecf6b650a11
vgabor96 <loahclan96@gmail.com>
2019-11-09 17:29:35 +0100

    Modifies according to new classes

--- src/com/sportsbetting/domain/Bet.java
+++ src/com/sportsbetting/domain/Bet.java
@@ -1,6 +1,9 @@
 package com.sportsbetting.domain;
 
+import java.util.List;
+
 public class Bet {
     String description;
     SportEvent event;
+    List<Outcome> outcomes;
 }
--- src/com/sportsbetting/domain/Result.java
+++ src/com/sportsbetting/domain/Result.java
@@ -1,4 +1,7 @@
 package com.sportsbetting.domain;
 
+import java.util.List;
+
 public class Result {
+    List<Outcome> winnerOutcomes;
 }
