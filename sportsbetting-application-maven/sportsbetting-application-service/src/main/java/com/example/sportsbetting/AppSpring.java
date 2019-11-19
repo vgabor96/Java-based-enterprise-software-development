110a638f7b1c8556b15107821480472c1928c5e6
vgabor96 <loahclan96@gmail.com>
2019-11-19 09:59:45 +0100

    AppSpring main method created

--- sportsbetting-application-maven/sportsbetting-application-service/src/main/java/com/example/sportsbetting/AppSpring.java
+++ sportsbetting-application-maven/sportsbetting-application-service/src/main/java/com/example/sportsbetting/AppSpring.java
@@ -1,14 +1,16 @@
 package com.example.sportsbetting;
 
 import com.example.sportsbetting.config.AppConfig;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppSpring {
+    public static void main(String[] args)
+    {
+        try (ConfigurableApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class)){
+            App app = appContext.getBean(App.class);
+            app.play();
+        }
+    }
 
-    public static void main(String[] args) {
-        try (ConfigurableApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class)) {
-            App app = appContext.getBean(App.class);
-            app.play();
-        }
-    }
 }
-
